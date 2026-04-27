d938da8ba26e8e4b55e153dc0114cf2691c8a322
Yokesh <yokeshbaskaran@gmail.com>
2026-04-27 23:47:47 +0530

    new modules added

---
+++ src/data/data.js
@@ -0,0 +1,10 @@
+const data = [
+  {
+    id: 1,
+    title: "Chat 1",
+    messages: [
+      { id: 1, text: "Hello", sender: "user" },
+      { id: 2, text: "Hi!", sender: "ai" },
+    ],
+  },
+];
